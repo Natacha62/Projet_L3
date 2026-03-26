012535db917037bf76e6e1682bb0d8706aa2306b
Natacha <natacha.cabelduc10@gmail.com>
2026-03-26 22:56:26 +0100

    Modification de l écoute du serveur (app.listen) dans server.js

--- server.js
+++ server.js
@@ -188,6 +188,7 @@ app.get('/', (req, res) => {
 });
 
 // Lancer le serveur
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running at http://0.0.0.0:${port}`);
 });
+
